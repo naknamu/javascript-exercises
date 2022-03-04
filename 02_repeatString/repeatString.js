7e20b4fc5ccc1e8e5938870c30360b28af64eb0e
naknamu <jodeldelvalle@gmail.com>
2022-03-04 11:02:03 +0800

    Added my answer and made it pass all the test required

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringValue, numberOfTimes) {
+    let output = "";
+    if (numberOfTimes < 0)
+    {
+        return 'ERROR'
+    }
+    for (let i = 0; i<numberOfTimes; i++)
+    {
+        output = output.concat(stringValue);
+    }
+    return output;
 };
 
 // Do not edit below this line

51f87e455548aa1a237a98922c6eaf452403d096
naknamu <jodeldelvalle@gmail.com>
2022-03-05 20:56:34 +0800

    Added correct answers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
 const sumAll = function() {
-
+    let sumNumber = 0;
+    if (!(Number.isInteger(arguments[0])) || !(Number.isInteger(arguments[1]))
+        || arguments[0] < 0 || arguments[1] < 0)
+    {
+        return 'ERROR'
+    } else {
+        if (arguments[0] > arguments[1])
+        {
+            for (let i=arguments[1]; i<= arguments[0]; i++)
+            {
+                sumNumber += i;
+            }
+        } else {
+            for (let i=arguments[0]; i<= arguments[1]; i++)
+            {
+                sumNumber += i;
+            }
+        }
+    }
+    return sumNumber;
 };
 
 // Do not edit below this line

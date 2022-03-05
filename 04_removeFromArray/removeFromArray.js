00dbea945c2335519ed552dad81201ea703f936f
naknamu <jodeldelvalle@gmail.com>
2022-03-05 20:36:37 +0800

    Added my answers and passed all the test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    const myArray = arguments[0];
+    let argsLength = arguments.length;
 
+    for (let j=1; j<argsLength; j++)
+    {
+        for (let i=0; i<myArray.length; i++)
+        {
+            if (arguments[j] === myArray[i])
+            {
+                myArray.splice(i,1);
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
